e4fe388bb674a272a5b7d186d6f18b3d62e0dab5
evannwong <43582348+evannwong@users.noreply.github.com>
2019-03-10 03:02:38 -0600

    Add files via upload
    added coins, goomba (barely functional and temporary), reduced character size to 30 pixels, added new environment platforms

--- Game.java
+++ Game.java
@@ -15,6 +15,10 @@ public class Game{
   Graphics graphics = new Graphics();
 
   private ArrayList<Node> platforms = new ArrayList<Node>();
+  private ArrayList<Node> coins = new ArrayList<Node>();
+  private ArrayList<Node> goombas = new ArrayList<Node>();
+  private ArrayList<Boolean> goombaTrue = new ArrayList<Boolean>();
+
 
   public HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
 
@@ -23,7 +27,9 @@ public class Game{
   private Pane menuRoot = new Pane();
 
   private Point2D playerVelocity = new Point2D(0, 0);
+  private Point2D goombaVelocity = new Point2D(0, 0);
   private boolean canJump = true;
+  private boolean goombaLeft = true;
 
   private int levelWidth;
 
@@ -43,6 +49,17 @@ public class Game{
             environmentRoot.getChildren().add(platform);
             platforms.add(platform);
             break;
+          case '2':
+            Node coin = graphics.createEntity(o*32, i*32, 32, 32, Color.YELLOW);
+            environmentRoot.getChildren().add(coin);
+            coins.add(coin);
+            break;
+          case '3':
+            Node goomba = graphics.createEntity(o*32, i*32, 32, 32, Color.BROWN);
+            environmentRoot.getChildren().add(goomba);
+            goombas.add(goomba);
+            goombaTrue.add(goombaLeft);
+            break;
         }
       }
     }
@@ -61,7 +78,24 @@ public class Game{
   }
 
   public void update(){
-    if (isPressed(KeyCode.W) && graphics.player.getTranslateY() >= 5){
+    for (int g = 0; g < goombaTrue.size(); g++){
+      for (int numG = 0; numG < goombas.size(); numG++){
+        if (goombas.get(numG).getTranslateX() >= 1 && goombaTrue.get(g) == true){
+          moveGoombaX(-1);
+        }
+        if (goombas.get(numG).getTranslateX() <= 1){
+          goombaTrue.set(g, false);
+        }
+        if (goombas.get(numG).getTranslateX() + 32 >= levelWidth - 5 && goombaTrue.get(g) == false){
+          goombaTrue.set(g, true);
+        }
+        if (goombaTrue.get(g) == false){
+          moveGoombaX(1);
+        }
+      }
+    }
+
+    if (isPressed(KeyCode.W)){
       jumpPlayer();
     }
 
@@ -72,17 +106,21 @@ public class Game{
     }
 
     if (isPressed(KeyCode.A) && graphics.player.getTranslateX() >= 5){
-      movePlayerX(-5);
+      movePlayerX(-3);
     }
 
-    if (isPressed(KeyCode.D) && graphics.player.getTranslateX() + 16 <= levelWidth - 5){
-      movePlayerX(5);
+    if (isPressed(KeyCode.D) && graphics.player.getTranslateX() + 32 <= levelWidth - 5){
+      movePlayerX(3);
     }
 
     if (playerVelocity.getY() < 10){
       playerVelocity = playerVelocity.add(0, 1);
     }
 
+    if (goombaVelocity.getY() < 10){
+      goombaVelocity = goombaVelocity.add(0, 1);
+    }
+    moveGoombaY((int) goombaVelocity.getY());
     movePlayerY((int) playerVelocity.getY());
   }
 
@@ -104,6 +142,11 @@ public class Game{
           }
         }
       }
+      for (Node coin : coins){
+        if (graphics.player.getBoundsInParent().intersects(coin.getBoundsInParent())){
+          coin.setVisible(false);
+        }
+      }
       graphics.player.setTranslateX(graphics.player.getTranslateX() + (movingRight ? 1 : -1));
     }
   }
@@ -114,13 +157,14 @@ public class Game{
       for (Node platform : platforms){
         if (graphics.player.getBoundsInParent().intersects(platform.getBoundsInParent())){
           if (movingDown){
-            if (graphics.player.getTranslateY() + 64 == platform.getTranslateY()){
+            if (graphics.player.getTranslateY() + 30 == platform.getTranslateY()){
               graphics.player.setTranslateY(graphics.player.getTranslateY() - 1);
               canJump = true;
               return;
             }
           } else{
-            if (graphics.player.getTranslateY() == platform.getTranslateY() + 32){
+            if (graphics.player.getTranslateY() == platform.getTranslateY() + 30){
+              graphics.player.setTranslateY(graphics.player.getTranslateY() + 1);
               playerVelocity = playerVelocity.add(0, 4);
               return;
             }
@@ -129,7 +173,71 @@ public class Game{
           canJump = false;
         }
       }
+      for (Node coin : coins){
+        if (graphics.player.getBoundsInParent().intersects(coin.getBoundsInParent())){
+          coin.setVisible(false);
+        }
+      }
       graphics.player.setTranslateY(graphics.player.getTranslateY() + (movingDown ? 1 : -1));
+    }
+  }
+
+  private void moveGoombaY(int value){
+    boolean movingDown = value > 0;
+    for (int i = 0; i < Math.abs(value); i++){
+      for (int numG = 0; numG < goombas.size(); numG++){
+        if (goombas.get(numG).getBoundsInParent().intersects(graphics.player.getBoundsInParent())){
+          if (goombas.get(numG).getTranslateY() - 20 >= graphics.player.getTranslateY()){
+            playerVelocity = playerVelocity.add(0, -32);
+            goombas.get(numG).setVisible(false);
+            goombas.get(numG).setTranslateY(goombas.get(numG).getTranslateY() + 100);
+          }
+        }
+        for (Node platform : platforms){
+          if (goombas.get(numG).getBoundsInParent().intersects(platform.getBoundsInParent())){
+            if (movingDown){
+              if (goombas.get(numG).getTranslateY() + 32 == platform.getTranslateY()){
+                goombas.get(numG).setTranslateY(goombas.get(numG).getTranslateY() - 1);
+                return;
+              }
+            } else{
+              if (goombas.get(numG).getTranslateY() == platform.getTranslateY() + 32){
+                goombaVelocity = goombaVelocity.add(0, 4);
+                return;
+              }
+            }
+          }
+        }
+        goombas.get(numG).setTranslateY(goombas.get(numG).getTranslateY() + (movingDown ? 1 : -1));
+      }
+    }
+  }
+
+  private void moveGoombaX(int value){
+    for (int i = 0; i < Math.abs(value); i++){
+      for (int g = 0; g < goombaTrue.size(); g++){
+        for (int numG = 0; numG < goombas.size(); numG++){
+          for (Node platform : platforms){
+            if (goombas.get(numG).getBoundsInParent().intersects(platform.getBoundsInParent())){
+              if (value > 0){
+                if (goombas.get(numG).getTranslateX() + 32 == platform.getTranslateX()){
+                  goombaTrue.set(g, true);
+                  goombas.get(numG).setTranslateX(goombas.get(numG).getTranslateX() - 1);
+                  return;
+                }
+              } else{
+                if (goombas.get(numG).getTranslateX() == platform.getTranslateX() + 32){
+                  goombaTrue.set(g, false);
+                  goombas.get(numG).setTranslateX(goombas.get(numG).getTranslateX() + 1);
+                  return;
+                }
+              }
+            }
+          }
+          goombas.get(numG).setTranslateX(goombas.get(numG).getTranslateX() + (goombaTrue.get(g) ? -1 : 1));
+          System.out.println(goombaTrue);
+        }
+      }
     }
   }
 
@@ -146,4 +254,3 @@ public class Game{
     return keys.getOrDefault(key, false);
   }
 }
-
--- Graphics.java
+++ Graphics.java
@@ -3,7 +3,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.Node;
 
 public class Graphics{
-  public Node player = createEntity(32, 536, 32, 64, Color.BLUE);
+  public Node player = createEntity(32, 536, 32, 30, Color.BLUE);
 
   public Node createEntity(int x, int y, int w, int h, Color color){
     Rectangle entity = new Rectangle(w, h);

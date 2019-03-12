0f0ea6044255313ce461a1367d7dfd396923e102
Robert McCurdy <RMcCurdy@users.noreply.github.com>
2019-03-12 13:29:53 -0600

    Update Graphics.java

--- UPDATED/Graphics.java
+++ UPDATED/Graphics.java
@@ -5,9 +5,10 @@ import javafx.scene.image.ImageView;
 
 
 public class Graphics{
-
+  /** An ImageView is created in order to pull in the specific image to use for the character. The width, height, x coord, and y coord is set inside of Game.java */
   ImageView player = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/mario.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
-
+  
+  /** Node created to set hit boxes and boundaries for the level*/
   public Node createEntity(int x, int y, int w, int h, Color color){
     Rectangle entity = new Rectangle(w, h);
     entity.setTranslateX(x);
@@ -16,6 +17,7 @@ public class Graphics{
     return entity;
   }
 
+  /** Node created to pull in the image to use for the brick. The dimensions and coordinates for the image are set inside of this method */
   public Node createBrick(int x, int y, int w, int h){
     ImageView entity = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/brick.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
     entity.setX(x);
@@ -25,6 +27,7 @@ public class Graphics{
     return entity;
   }
 
+  /** Node created to pull in the image to use for the coin. The dimensions and coordinates for the image are set inside of this method */
   public Node createCoin(int x, int y, int w, int h){
     ImageView coin = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/frame1-sky3.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
     coin.setX(x);
@@ -33,7 +36,8 @@ public class Graphics{
     coin.setFitHeight(h);
     return coin;
   }
-
+  
+  /** Node created to pull in the image to use for the ground. The dimensions and coordinates for the image are set inside of this method */
   public Node createGround(int x, int y, int w, int h){
     ImageView ground = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/ground64by64.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
     ground.setX(x);
@@ -43,15 +47,7 @@ public class Graphics{
     return ground;
   }
 
-  /**public Node createGoomba(int x, int y, int w, int h){
-    ImageView goomba = new ImageView("https://static.giantbomb.com/uploads/original/9/93854/2438851-goomba+smb+sprite+walk+gif.gif");
-    goomba.setX(x);
-    goomba.setY(y);
-    goomba.setFitWidth(w);
-    goomba.setFitHeight(h);
-    return goomba;
-  }*/
-
+  /** Node created to pull in the image to use for the cloud. The dimensions and coordinates for the image are set inside of this method */
   public Node createCloud(int x, int y, int w, int h){
     ImageView cloud = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/cloud32by64.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
     cloud.setX(x);
@@ -61,6 +57,7 @@ public class Graphics{
     return cloud;
   }
 
+  /** Node created to pull in the image to use for the background. The dimensions and coordinates for the image are set inside of this method */
   public Node createBack(int x, int y, int w, int h){
     ImageView back = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/background-sky3.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
     back.setX(x);
@@ -70,6 +67,7 @@ public class Graphics{
     return back;
   }
 
+  /** Node created to pull in the image to use for the middle of the pipe. The dimensions and coordinates for the image are set inside of this method */
   public Node createPipeMain(int x, int y, int w, int h){
     ImageView pipeM = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/pipemain.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
     pipeM.setX(x);
@@ -79,12 +77,13 @@ public class Graphics{
     return pipeM;
   }
 
-public Node createPipeTop(int x, int y, int w, int h){
-  ImageView pipeT = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/pipetop.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
-  pipeT.setX(x);
-  pipeT.setY(y);
-  pipeT.setFitWidth(w);
-  pipeT.setFitHeight(h);
-  return pipeT;
+  /** Node created to pull in the image to use for the top of the pipe. The dimensions and coordinates for the image are set inside of this method */
+  public Node createPipeTop(int x, int y, int w, int h){
+    ImageView pipeT = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/pipetop.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
+    pipeT.setX(x);
+    pipeT.setY(y);
+    pipeT.setFitWidth(w);
+    pipeT.setFitHeight(h);
+    return pipeT;
   }
 }

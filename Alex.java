4e39d971ad18e8f493ad6d37d8ff355ecc504611
evannwong <43582348+evannwong@users.noreply.github.com>
2019-04-03 00:24:05 -0600

    Add files via upload

--- Alex.java
+++ Alex.java
@@ -13,9 +13,8 @@ import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 
 public class Alex extends Game{
-  private Point2D playerVelocity = new Point2D(0, 0); // X and Y momentum
+  protected static Point2D playerVelocity = new Point2D(0, 0); // X and Y momentum
   private boolean canJump = true;
-
 
   /**
   *Checks if any key is pressed, and returns either the key itself if it is true, or false otherwise.
@@ -29,11 +28,15 @@ public class Alex extends Game{
   */
   @Override
   public void update(){
+    coinCount.setText(coinCounter + "");
+    if (graphics.player.getTranslateY() > 650){
+      die();
+    }
     if (isPressed(KeyCode.W)){
       jumpPlayer();
     }
 
-    if (isPressed(KeyCode.SHIFT) && isPressed(KeyCode.D) && playerVelocity.getX() <= 4 && graphics.player.getTranslateX() + 32 <= levelWidth - 5){
+    if (isPressed(KeyCode.SHIFT) && isPressed(KeyCode.D) && playerVelocity.getX() <= 4 && graphics.player.getTranslateX() + 28 <= levelWidth - 5){
       playerVelocity = playerVelocity.add(0.65, 0);
     }
 
@@ -51,7 +54,7 @@ public class Alex extends Game{
       movePlayerX(-3);
     }
 
-    if (isPressed(KeyCode.D) && graphics.player.getTranslateX() + 32 <= levelWidth - 5){
+    if (isPressed(KeyCode.D) && graphics.player.getTranslateX() + 28 <= levelWidth - 5){
       movePlayerX(3);
     }
 
@@ -68,6 +71,9 @@ public class Alex extends Game{
       playerVelocity = playerVelocity.add(-0.25, 0);
     }
     movePlayerX((int) playerVelocity.getX());
+
+    /** Command to output the updated x-coordinate of the player in pixels */
+    //System.out.println(graphics.player.getTranslateX());
   }
 
   /**
@@ -92,7 +98,7 @@ public class Alex extends Game{
       for (Node platform : platforms){
         if (graphics.player.getBoundsInParent().intersects(platform.getBoundsInParent())){
           if (movingRight){
-            if (graphics.player.getTranslateX() + 32 == platform.getTranslateX()){
+            if (graphics.player.getTranslateX() + 28 == platform.getTranslateX()){
               graphics.player.setTranslateX(graphics.player.getTranslateX() - 1);
               return;
             }
@@ -104,9 +110,26 @@ public class Alex extends Game{
           }
         }
       }
+      for (Node platform2 : platforms){
+        if (graphics.player.getBoundsInParent().intersects(platform2.getBoundsInParent())){
+          if (movingRight){
+            if (graphics.player.getTranslateX() + 28 == platform2.getTranslateX()){
+              graphics.player.setTranslateX(graphics.player.getTranslateX() - 1);
+              return;
+            }
+          } else{
+            if (graphics.player.getTranslateX() == platform2.getTranslateX() + 28){
+              graphics.player.setTranslateX(graphics.player.getTranslateX() + 1);
+              return;
+            }
+          }
+        }
+      }
       for (Node coin : coins){
         if (graphics.player.getBoundsInParent().intersects(coin.getBoundsInParent())){
           coin.setVisible(false);
+          coin.setTranslateY(900);
+          coinCounter += 1;
         }
       }
       graphics.player.setTranslateX(graphics.player.getTranslateX() + (movingRight ? 1 : -1));
@@ -123,7 +146,7 @@ public class Alex extends Game{
       for (Node platform : platforms){
         if (graphics.player.getBoundsInParent().intersects(platform.getBoundsInParent())){
           if (movingDown){
-            if (graphics.player.getTranslateY() + 30 == platform.getTranslateY()){
+            if (graphics.player.getTranslateY() + 60 == platform.getTranslateY()){
               graphics.player.setTranslateY(graphics.player.getTranslateY() - 1);
               canJump = true;
               return;
@@ -139,12 +162,56 @@ public class Alex extends Game{
           canJump = false;
         }
       }
+      for (Node platform2 : platforms){
+        if (graphics.player.getBoundsInParent().intersects(platform2.getBoundsInParent())){
+          if (movingDown){
+            if (graphics.player.getTranslateY() + 60 == platform2.getTranslateY()){
+              graphics.player.setTranslateY(graphics.player.getTranslateY() - 1);
+              canJump = true;
+              return;
+            }
+          } else{
+            if (graphics.player.getTranslateY() == platform2.getTranslateY() + 30){
+              graphics.player.setTranslateY(graphics.player.getTranslateY() + 1);
+              playerVelocity = playerVelocity.add(0, 4);
+              return;
+            }
+          }
+        } else{
+          canJump = false;
+        }
+      }
       for (Node coin : coins){
         if (graphics.player.getBoundsInParent().intersects(coin.getBoundsInParent())){
           coin.setVisible(false);
+          coin.setTranslateY(900);
+          coinCounter += 1;
+        }
+      }
+      for (int numG = 0; numG < goombas.size(); numG++){
+        if (goombas.get(numG).getBoundsInParent().intersects(graphics.player.getBoundsInParent())){
+          if (goombas.get(numG).getTranslateY() - 35 >= graphics.player.getTranslateY()){
+            if (playerVelocity.getY() < -7){
+              playerVelocity = playerVelocity.add(0, 4);
+            } else{
+              playerVelocity = new Point2D(0, 0);
+              playerVelocity = playerVelocity.add(0, -4);
+            }
+          }
+          if (graphics.player.getTranslateX() == goombas.get(numG).getTranslateX() + 28){
+            die();
+          } else if (graphics.player.getTranslateX() + 28 == goombas.get(numG).getTranslateX()){
+            die();
+          }
         }
       }
       graphics.player.setTranslateY(graphics.player.getTranslateY() + (movingDown ? 1 : -1));
     }
+  }
+
+  public void die(){
+    graphics.player.setTranslateX(30);
+    graphics.player.setTranslateY(500);
+    environmentRoot.setLayoutX(0);
   }
 }
--- Game.java
+++ Game.java
@@ -11,6 +11,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import java.io.*;
 
 public abstract class Game{
   Graphics graphics = new Graphics();
@@ -18,30 +21,91 @@ public abstract class Game{
   protected ArrayList<Node> platforms = new ArrayList<Node>(); //List of all platforms in the environment.
   protected ArrayList<Node> coins = new ArrayList<Node>(); //List of all coins in the environment.
   protected ArrayList<Node> goombas = new ArrayList<Node>(); // List of all goombas.
-  protected ArrayList<Boolean> goombaLeft = new ArrayList<Boolean>(); // List of all goomba's direction.
+  protected ArrayList<Boolean> goombaTrue = new ArrayList<Boolean>(); // List of all goomba's direction.
+  protected ArrayList<Point2D> goombaGravity = new ArrayList<Point2D>(); // List of all goomba's gravity vectors.
 
   public HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
 
+  protected boolean goombaLeft = true;
+  protected Point2D goombaVelocity = new Point2D(0, 0);
 
   protected Pane appRoot = new Pane();
-  private Pane environmentRoot = new Pane();
+  protected Pane environmentRoot = new Pane();
   private Pane menuRoot = new Pane();
 
-  protected int levelWidth;
+  protected int levelWidth = 0;
+  protected int levelHeight = 0;
+  protected int coinCounter = 0;
+  protected Text coinCount;
+  String line = "";
 
   /**
   *Method for initializing the game window, and the environment.
   */
-  public void initContent(){
-    appRoot.setPrefSize(800, 320);
-    environmentRoot.setPrefSize(800, 320);
-    menuRoot.setPrefSize(800, 320);
+  public void initContent(int level, int skin){
+    /**
+    * Checks for a save file, and loads in the amount of coins
+    */
+    try{
+      FileReader file = new FileReader("info.txt");
+      BufferedReader reader = new BufferedReader(file);
+      coinCounter = Integer.parseInt(reader.readLine());
+    } catch(IOException ioe){
+      coinCounter = 0;
+    }
 
+    appRoot.setPrefSize(800, 640);
+    environmentRoot.setPrefSize(800, 640);
+    menuRoot.setPrefSize(800, 640);
 
-    levelWidth = Environment.LEVEL1[0].length() * 32; // Checks the array in Environment.java and for each number, adds a different graphic.
+    ImageView coinLogo = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/frame1-sky3.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
+    coinLogo.setFitWidth(20);
+    coinLogo.setFitHeight(20);
+    coinLogo.setX(700);
+    coinLogo.setY(20);
+    menuRoot.getChildren().add(coinLogo);
+    coinCount = new Text(750, 40, coinCounter + "");
+    coinCount.setFont(Font.font ("Verdana", 20));
+    menuRoot.getChildren().add(coinCount);
 
-    for (int i = 0; i < Environment.LEVEL1.length; i++){
-      String line = Environment.LEVEL1[i];
+    if (level == 1){
+      levelWidth = Environment.LEVEL1[0].length() * 32;
+      levelHeight = Environment.LEVEL1.length;
+    }
+    if (level == 2){
+      levelWidth = Environment.LEVEL2[0].length() * 32;
+      levelHeight = Environment.LEVEL2.length;
+    }
+    if (level == 3){
+      levelWidth = Environment.LEVEL3[0].length() * 32;
+      levelHeight = Environment.LEVEL3.length;
+    }
+    if (level == 4){
+      levelWidth = Environment.LEVEL4[0].length() * 32;
+      levelHeight = Environment.LEVEL4.length;
+    }
+    if (level == 5){
+      levelWidth = Environment.LEVEL5[0].length() * 32;
+      levelHeight = Environment.LEVEL5.length;
+    }
+
+    for (int i = 0; i < levelHeight; i++){
+      System.out.println("Loading: " + i*5);
+      if (level == 1){
+        line = Environment.LEVEL1[i];
+      }
+      if (level == 2){
+        line = Environment.LEVEL2[i];
+      }
+      if (level == 3){
+        line = Environment.LEVEL3[i];
+      }
+      if (level == 4){
+        line = Environment.LEVEL4[i];
+      }
+      if (level == 5){
+        line = Environment.LEVEL5[i];
+      }
       for (int o = 0; o < line.length(); o++){
         switch(line.charAt(o)){
           case '0':
@@ -52,14 +116,24 @@ public abstract class Game{
             environmentRoot.getChildren().add(ground);
             platforms.add(platform);
             break;
+          case 'G':
+            Node Fground = graphics.createFinalGround(o*32, i*32, 32, 32);
+            Node Fplatform = graphics.createEntity(o*32, i*32, 32, 32, Color.BROWN);
+            environmentRoot.getChildren().add(Fground);
+            platforms.add(Fplatform);
+            break;
           case '2':
             Node coin = graphics.createCoin(o*32, i*32, 32, 32);
             environmentRoot.getChildren().add(coin);
             coins.add(coin);
             break;
           case '3':
-            Node back = graphics.createBack(0, 0, 3200, 320);
+            Node back = graphics.createBack(0, 0, 3200, 640);
             environmentRoot.getChildren().add(back);
+            break;
+          case 'B':
+            Node Fback = graphics.createFinalBack(0, 0, 3200, 640);
+            environmentRoot.getChildren().add(Fback);
             break;
           case '4':
             Node brick = graphics.createBrick(o*32, i*32, 32, 32);
@@ -67,10 +141,20 @@ public abstract class Game{
             environmentRoot.getChildren().add(brick);
             platforms.add(platform2);
             break;
+          case 'P':
+            Node Fbrick = graphics.createFinalBrick(o*32, i*32, 32, 32);
+            Node Fplatform2 = graphics.createEntity(o*32, i*32, 32, 32, Color.BROWN);
+            environmentRoot.getChildren().add(Fbrick);
+            platforms.add(Fplatform2);
+            break;
           case '5':
             Node cloud = graphics.createCloud(o*32, i*32, 64, 32);
             environmentRoot.getChildren().add(cloud);
-            platforms.add(cloud);
+            break;
+          case 'F':
+            Node pole = graphics.createFlagPole(o*32, i*32, 32, 256);
+            environmentRoot.getChildren().add(pole);
+            coins.add(pole);
             break;
           case '6':
             Node pipeMain = graphics.createPipeMain(o*32, i*32, 32, 32);
@@ -85,15 +169,35 @@ public abstract class Game{
             platforms.add(platform4);
             break;
           case '8':
+            Node goomba = graphics.createEnemy(o*32, i*32);
+            environmentRoot.getChildren().add(goomba);
+            goombas.add(goomba);
+            goombaTrue.add(goombaLeft);
+            goombaGravity.add(goombaVelocity);
             break;
         }
       }
     }
 
+    if (skin == 1){
+      graphics.player = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/alejandro-1.png");
+    }
+    if (skin == 2){
+      graphics.player = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/alejandro-2.png");
+    }
+    if (skin == 3){
+      graphics.player = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/alejandro-3.png");
+    }
+    if (skin == 4){
+      graphics.player = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/alejandro-4.png");
+    }
+    if (skin == 5){
+      graphics.player = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/alejandro-5.png");
+    }
     graphics.player.setTranslateX(30);
-    graphics.player.setTranslateY(100);
-    graphics.player.setFitWidth(32);
-    graphics.player.setFitHeight(30);
+    graphics.player.setTranslateY(500);
+    graphics.player.setFitWidth(28);
+    graphics.player.setFitHeight(60);
     environmentRoot.getChildren().add(graphics.player);
 
     graphics.player.translateXProperty().addListener((obs, old, newValue) -> {

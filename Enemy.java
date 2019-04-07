e4b9e7249f6eb44ef016d235cbcd2cbebbec66d4
Robert McCurdy <RMcCurdy@users.noreply.github.com>
2019-04-07 14:05:02 -0600

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -27,7 +27,7 @@ public class Enemy extends Alex{
         if (goombas.get(numG).getTranslateY() - 35 >= graphics.player.getTranslateY()){
           goombas.get(numG).setVisible(false);
           goombas.get(numG).setTranslateY(goombas.get(numG).getTranslateY() + 100);
-          System.out.println("Defeated a goomba");
+          System.out.println("\nYou defeated a goomba!\n");
         }
       }
       if (goombas.get(numG).getTranslateX() >= 1 && goombaTrue.get(numG) == true){
--- Graphics.java
+++ Graphics.java
@@ -8,7 +8,6 @@ public class Graphics{
   /** An ImageView is created in order to pull in the specific image to use for the character. The width, height, x coord, and y coord is set inside of Game.java */
   ImageView player = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/alejandro-1.png?token=ApkDjG3QpMdkdKtqJ4uwSCDIybeWTQbJks5cjg1RwA%3D%3D");
 
-
   /** Node created to pull in the image to use for the enemy. The dimensions and coordinates for the image are set inside of this method */
   public ImageView createEnemy(int x, int y){
     ImageView entity = new ImageView("https://raw.githubusercontent.com/RMcCurdy/TeamProjectGroup14/master/Images/goombaFIX.png");
